9e0e3263c00f34ae132f4c252a974f6a4cf5319b
Hr. Nelson <gamle_dirk@jehovasvidner>
2018-05-19 12:07:52 +0200

    recent

--- I4DABH4/I4DABH4/Data/Traderinfo/UnitOfWork.cs
+++ I4DABH4/I4DABH4/Data/Traderinfo/UnitOfWork.cs
@@ -12,7 +12,7 @@ namespace I4DABH4.Data.Traderinfo
         private readonly DocumentClient _client;
         readonly string _host = "https://localhost:8081";
         readonly string _key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
-        readonly string _dbname = "TestDB";
+        readonly string _dbname = "TraderInfo";
         readonly string _collection = "TestCollection";
         private readonly Uri _collectionUri;
        // private PersonRepository _personRepository;
